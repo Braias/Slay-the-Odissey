0103aa56010f2773b70f4563591143826228b477
nmspaniol <nicolasmspaniol@gmail.com>
2024-11-02 16:03:11 -0300

    Continua a definição de classes

--- src/entities.py
+++ src/entities.py
@@ -1,17 +1,36 @@
 import pygame
+
+
 class Entity():
-    def __init__(self,life:int,deck:dict,
-                 sprite:pygame.image,name:str,xp:int,coins:int,
-                 actions:int):
-        self.life = life
+    def __init__(self, life: int, deck: Deck, sprite: pygame.image, name: str, energy: int):
         self.defense = 0
         self.is_alive = True
+        self.life = life
         self.deck = deck
         self.sprite = sprite
-        self.hand = []
         self.name = name
-        self.xp = xp
-        self.coins = coins
-        self.actions = actions
+        self.energy = energy
 
 
+class Enemy():
+    def __init__(self, drop_xp: int):
+        self.drop_xp = drop_xp
+
+
+class Ulisses():
+    def __init__(self):
+        self.level = 0
+        self.xp = 0
+        self.coins = 0
+
+
+class Deck():
+    def __init__(self):
+        self.hand = []
+        self.draw_pile = []
+        self.discard_pile = []
+
+    def shuffle(self):
+        # TODO
+
+        return hand
