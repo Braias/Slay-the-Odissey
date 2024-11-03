82fd0b0ba86be53796892157099b2131e23b0608
Nicholas2104 <nicholasnogueira.costa@gmail.com>
2024-11-03 11:07:41 -0300

    Add: Metodo Shuffle, Atributo max_hp

---
+++ src/cards.py
@@ -0,0 +1,19 @@
+import random
+
+class Deck():
+    def __init__(self,draw_pile=[]):
+        self.hand = []
+        self.draw_pile = draw_pile
+        self.discard_pile = []
+
+    def shuffle_and_allocate(self):     
+        # Checamos quantas caratas estão disponíveis
+        cards_to_draw = len(self.draw_pile) 
+        # Caso não temos cartas suficentes para formar uma mão adcionamos do deck de descarte
+        if cards_to_draw < 5: 
+            self.draw_pile+=self.discard_pile
+        random.shuffle(self.draw_pile)
+        hand = self.draw_pile[:5]
+        return hand
+
+
--- src/entities.py
+++ src/entities.py
@@ -1,11 +1,13 @@
 import pygame
+from cards import Deck
 
 
 class Entity():
-    def __init__(self, life: int, deck: Deck, sprite: pygame.image, name: str, energy: int):
+    def __init__(self, max_hp: int, deck: Deck, sprite: pygame.image, name: str, energy: int):
         self.defense = 0
         self.is_alive = True
-        self.life = life
+        self.current_life = max_hp
+        self.max_hp = max_hp
         self.deck = deck
         self.sprite = sprite
         self.name = name
@@ -22,15 +24,3 @@ class Ulisses():
         self.level = 0
         self.xp = 0
         self.coins = 0
-
-
-class Deck():
-    def __init__(self):
-        self.hand = []
-        self.draw_pile = []
-        self.discard_pile = []
-
-    def shuffle(self):
-        # TODO
-
-        return hand
