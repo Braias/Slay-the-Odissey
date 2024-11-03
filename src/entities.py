4028a61f272504d5f364de5e52db5661fce7a951
Nicholas2104 <nicholasnogueira.costa@gmail.com>
2024-11-03 16:27:36 -0300

    Estabelecendo hierarquia de clases e adcionando comentarios

--- src/entities.py
+++ src/entities.py
@@ -3,6 +3,19 @@ from cards import Deck
 
 
 class Entity():
+    """
+    Classe que representa uma entidade qualquer no jogo
+
+    Atributos:
+        defense (int): Valor da defesa da entidade, que reduz o dano recebido
+        is_alive (bool): Indica se a entidade está viva
+        current_life (int): Quantidade de pontos de vida atuais da entidade
+        max_hp (int): Quantidade máxima de pontos de vida da entidade
+        deck (Deck): Conjunto de cartas associado à entidade
+        sprite (pygame.image): Sprite reprsentativo da entidade
+        name (str): Nome da entidade
+        energy (int): Quantidade energia disponivel para aplicar cartas
+    """
     def __init__(self, max_hp: int, deck: Deck, sprite: pygame.image, name: str, energy: int):
         self.defense = 0
         self.is_alive = True
@@ -14,12 +27,26 @@ class Entity():
         self.energy = energy
 
 
-class Enemy():
+class Enemy(Entity):
+    """
+    Classe que representa um inimigo no jogo - herda classe 'Entity'
+
+    Atributos:
+        drop_xp (int): Quantidade de experiência que o inimigo dropa na morte
+    """
     def __init__(self, drop_xp: int):
         self.drop_xp = drop_xp
 
 
-class Ulisses():
+class Ulisses(Entity):
+    """
+    Classe que representa o personagem principal - herdando da classe 'Entity'.
+
+    Atributos:
+        level (int): Nível atual do personagem
+        xp (int): Experiência acumulada do personagem
+        coins (int): Quantidade de moedas que o personagem possui
+    """
     def __init__(self):
         self.level = 0
         self.xp = 0
