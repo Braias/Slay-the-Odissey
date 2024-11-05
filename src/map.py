2202c71d89b59ba57d5844ef8c7f007016888140
nmspaniol <nicolasmspaniol@gmail.com>
2024-11-05 14:39:00 -0300

    Divisão de responsabilidades das classes

--- src/map.py
+++ src/map.py
@@ -1,7 +1,7 @@
 """ Módulo responsável pelas mecânicas do mapa do jogo """
 
 
-from pygame import Surface, draw, display, math
+from pygame import Surface, draw, display, math, image
 from map_node import MapNode, MapNodeType, Point
 
 
@@ -11,7 +11,7 @@ SCROLL_SPEED = 2
 MARGIN = 20
 
 COLOR_MAP = (200, 200, 150)
-
+      
 
 class MapView(Surface): # TODO: implicações de herdar Surface
     def __init__(self, target: Surface, root: MapNode):
@@ -30,13 +30,15 @@ class MapView(Surface): # TODO: implicações de herdar Surface
         self._add_children(root)
 
         self.current_node = root
-        root.visit()
+        root.begin()
 
         self.scrolling = False
         self.scroll_initial_y = 0
 
         self.scroll_interval = (target.get_height() - MAP_HEIGHT - MARGIN, MARGIN)
         self.focus_on(root)
+
+        self.spritesheet = image.load("assests/map_icons.png")
 
     def mouse_motion(self, mouse_pos: Point):
         if self.scrolling:
@@ -50,31 +52,24 @@ class MapView(Surface): # TODO: implicações de herdar Surface
             )
 
         for node in self.nodes:
+            if not node.is_navigable: continue
+            
             diff = (node.pos[0] - mouse_pos[0] + self.pos[0]) ** 2 + \
                    (node.pos[1] - mouse_pos[1] + self.pos[1]) ** 2
 
-            if (diff < 400) and node.is_available:
-                node.is_hovered = True
+            if (diff < 400):
                 self.hovered_node = node
                 return
 
         # Aqui o mouse não está sobre nenhum nó
         
-        if self.hovered_node != None:
-            self.hovered_node.is_hovered = False
-
         self.hovered_node = None
 
     def mouse_down(self, mouse_pos: Point):
-        if self.hovered_node != None and self.hovered_node.is_available:
-            self.current_node.is_active = False
-            self.current_node.is_available = False
-
-            for child in self.current_node.children:
-                child.is_available = False
-
-            self.hovered_node.visit()
+        if self.hovered_node != None and self.hovered_node.is_navigable:
+            self.current_node.navigate_to(self.hovered_node)
             self.current_node = self.hovered_node
+            self.hovered_node = None
 
             return self.current_node.data
         else:
@@ -93,7 +88,7 @@ class MapView(Surface): # TODO: implicações de herdar Surface
         self._render_edges(self.root)
 
         for node in self.nodes:
-            node.render(self)
+            self._render_node(node)
 
         self.target.blit(self, (self.pos[0], self.pos[1]))
 
@@ -114,30 +109,51 @@ class MapView(Surface): # TODO: implicações de herdar Surface
 
     def _render_edges(self, origin: MapNode):
         for child in origin.children:
-            if (child.was_visited or child.is_available) and origin.was_visited:
+            if (child.was_visited or child.is_navigable) and origin.was_visited:
                 draw.line(self, (100, 100, 60), origin.pos, child.pos, 4)
             else:
                 draw.line(self, (150, 150, 110), origin.pos, child.pos, 4)
 
             self._render_edges(child)
 
+    def _render_node(self, node: MapNode):
+        color = (100, 100, 100)
+        if node.was_visited or node.is_navigable:
+            color = (0, 0, 0)
+
+        size = 10 if node.is_navigable else 6
+
+        if node.type == MapNodeType.BATTLE:
+            draw.circle(self, color, node.pos, size)
+        elif node.type == MapNodeType.STORY:
+            draw.rect(self, color, (
+                node.pos[0] - size, node.pos[1] - size,
+                size << 1, size << 1
+            ))
+        elif node.type == MapNodeType.BOSS:
+            draw.circle(self, color, node.pos, size * 3)
+
+        if node == self.hovered_node: # overlay
+            draw.circle(self, (140, 140, 100), node.pos, size + 16, width = 4)
+        
+
 
 # ------------------------------------
 
 
-node = MapNode((230, 400), MapNodeType.STORY, None)
+node = MapNode((230, 400), MapNodeType.STORY, 1)
 
-odyssey_map = MapNode((200, 540), MapNodeType.BATTLE, None).to(
-    MapNode((170, 460), MapNodeType.STORY, None).to(
-        MapNode((150, 360), MapNodeType.BATTLE, None),
+odyssey_map = MapNode((200, 540), MapNodeType.BATTLE, 2).to(
+    MapNode((170, 460), MapNodeType.STORY, 3).to(
+        MapNode((150, 360), MapNodeType.BATTLE, 4),
         node.to(
-            MapNode((180, 290), MapNodeType.BATTLE, None).to(
-                MapNode((220, 170), MapNodeType.BOSS, None)
+            MapNode((180, 290), MapNodeType.BATTLE, 5).to(
+                MapNode((220, 170), MapNodeType.BOSS, 6)
             )
         )
     ),
-    MapNode((210, 490), MapNodeType.BATTLE, None).to(
-        MapNode((260, 440), MapNodeType.STORY, None).to(
+    MapNode((210, 490), MapNodeType.BATTLE, 7).to(
+        MapNode((260, 440), MapNodeType.STORY, 8).to(
             node
         )
     )
--- src/map_node.py
+++ src/map_node.py
@@ -7,10 +7,6 @@ from typing import Self
 Point = tuple[int, int]
 
 
-class Scroller:
-    pass
-
-
 class MapNodeType(Enum):
     BATTLE = 0
     STORY = 1
@@ -23,44 +19,31 @@ class MapNode(ABC):
         self.children = []
         self.type = type
         self.data = data
-        self.reset_flags()
-
-    def reset_flags(self):
         self.was_visited = False
-        self.is_available = False
-        self.is_hovered = False
+        self.is_navigable = False
         self.is_active = False
 
     def to(self, *args: list[Self]):
         self.children += args
         return self
 
-    def render(self, surface: Surface):
-        color = (100, 100, 100)
-        if self.was_visited or self.is_available:
-            color = (0, 0, 0)
+    def navigate_to(self, child: Self):
+        if child not in self.children:
+            raise ValueError("Nó sendo visitado não é filho do anterior")
 
-        size = 10 if self.is_available else 6
+        self.is_active = False
+        for c in self.children:
+            c.is_navigable = False
 
-        if self.type == MapNodeType.BATTLE:
-            draw.circle(surface, color, self.pos, size)
-        elif self.type == MapNodeType.STORY:
-            draw.rect(surface, color, (
-                self.pos[0] - size, self.pos[1] - size,
-                size << 1, size << 1
-            ))
-        elif self.type == MapNodeType.BOSS:
-            draw.circle(surface, color, self.pos, size * 3)
+        child.was_visited = True
+        child.is_active = True
+        child.is_navigable = False
 
-        if self.is_hovered: # overlay
-            draw.circle(surface, (140, 140, 100), self.pos, size + 16, width = 4)
+        for c in child.children:
+            c.is_navigable = True
 
-    def visit(self):
+    def begin(self):
         self.was_visited = True
         self.is_active = True
-        self.is_hovered = False
-        self.is_available = False
-
-        for child in self.children:
-            child.is_available = True
-
+        for c in self.children:
+            c.is_navigable = True
