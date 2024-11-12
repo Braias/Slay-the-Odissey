6157090fc02c363d3592cba36d2605775867173d
Sofia <sofia.moumont@gmail.com>
2024-11-12 18:10:22 -0300

    Teste da função check_target OK

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -26,7 +26,7 @@ class TestCard(unittest.TestCase):
         test_energy_card.check_energy(self.owner)
         
         # Iserir aqui a lógica de tratamento do erro pro inimigo
-        mock_print.assert_called_once_with("Ei, inimigo!")
+        mock_print.assert_called_once_with("Ei, inimigo")
 
     @unittest.mock.patch("builtins.print")
     def test_check_energy_player_logic(self, mock_print):
@@ -36,18 +36,24 @@ class TestCard(unittest.TestCase):
         test_energy_card.check_energy(self.owner)
         
         # Iserir aqui a lógica de tratamento do erro pro ulisses
-        mock_print.assert_called_once_with("Ei, Ulisses!")
+        mock_print.assert_called_once_with("Ei, Ulisses")
         
     
     @unittest.mock.patch("builtins.print")
     def test_wrong_target_selected_for_attack(self, mock_print):
-        self.owner = "Ulisses"
-        self.target = "Ulisses"
         
         test_wrong_target_selected_for_attack_card = cards.AttackCard("Tapa", "Estapeia o inimigo", cost=1, damage=2)
-        test_wrong_target_selected_for_attack_card.check_target(self.target, self.owner)
+        test_wrong_target_selected_for_attack_card.check_target(self.owner, self.owner)
         
-        mock_print.assert_called_once_with("Essa carta não pode ser aplicada em si mesma")
+        mock_print.assert_called_once_with("Essa carta não pode ser aplicada em si mesmo")
+    
+    @unittest.mock.patch("builtins.print")
+    def test_wrong_target_selected_for_defense(self, mock_print):
+        
+        test_wrong_target_selected_for_defense_card = cards.DefenseCard("Escudo", "Equipa um escudo", cost=1, defense=2)
+        test_wrong_target_selected_for_defense_card.check_target(self.owner, self.target)
+        
+        mock_print.assert_called_once_with("Essa carta não pode ser aplicada em um inimigo")
     
     def test_damage_bigger_than_defense(self):
         pass
