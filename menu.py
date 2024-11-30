26b44b0299de83bcbbd88ab547fc0821d4c5079b
João Vitor <joaovtaf@gmail.com>
2024-11-30 18:16:40 -0300

    remodelei o menu e a classe de botões

--- menu.py
+++ menu.py
@@ -1,74 +1,69 @@
-import pygame, sys
-from button import Button
+"""Módulo responsável pelo menu inicial do jogo."""
 
-pygame.init()
 
-SCREEN = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Menu")
+import pygame
+import sys
+from menu_button import Button
+from screen import Screen
 
-BG = pygame.image.load("assets/artRio.png")
 
-def get_font(size): # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("assets/font.ttf", size)
+class MenuScreen(Screen):
+    def __init__(self, surface: pygame.Surface, map):
+        self._load_sprites()
+        self.pos = pygame.Vector2(surface.get_size())
+        self.pos -= self.menu_sprite.get_size()
+        self.pos /= 2
+        self.map = map
+        self.buttons = []
+        self.pressed_play = False
+        self.surface = surface
+        self.title_surface = pygame.font.Font("assets/font.ttf", 50).render("Slay the Odissey", True, "#b68f40")
+        self.title_pos = (self.surface.get_width() / 2 - self.title_surface.get_width() / 2, 30)
 
-def play():
-    while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        self.buttons.append(Button(
+            (self.surface.get_width() / 2, 140),
+            (200, 50),
+            "Jogar",
+            self.font
+        ))
+        self.buttons.append(Button(
+            (self.surface.get_width() / 2, 220),
+            (200, 50),
+            "Sair",
+            self.font
+        ))
 
-        SCREEN.fill("black")
+        self.surface = surface
 
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 400))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
-
-        PLAY_BACK = Button(image=None, pos=(640, 460), 
-                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    def handle_event(self, ev: pygame.event.Event):
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            if self.buttons[0].is_hovering:
+                self.pressed_play = True
+            if self.buttons[1].is_hovering:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    main_menu()
+        elif ev.type == pygame.MOUSEMOTION:
+            for button in self.buttons:
+                button.on_mouse_motion(ev.dict["pos"])
 
-        pygame.display.update()
+    def update(self):
+        if self.pressed_play:
+            return self.map
     
+    def onenter(self):
+        pass
 
-def main_menu():
-    while True:
-        SCREEN.blit(BG, (0, 0))
-
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
-
-        MENU_TEXT = get_font(100).render("Slay the Odissey", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
-
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 300), 
-                            text_input="Play", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 420), 
-                            text_input="Quit", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-
-        SCREEN.blit(MENU_TEXT, MENU_RECT)
-
-        for button in [PLAY_BUTTON, QUIT_BUTTON]:
-            button.changeColor(MENU_MOUSE_POS)
-            button.update(SCREEN)
+    def draw(self):
+        self.surface.fill((0,0,0))
+        self.surface.blit(self.menu_sprite, self.pos)
+        self.surface.blit(self.title_surface, self.title_pos)
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    play()
-                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    pygame.quit()
-                    sys.exit()
-
-        pygame.display.update()
-
-main_menu()
+        for button in self.buttons:
+            button.draw(self.surface)
+            
+    def _load_sprites(self):
+        self.menu_sprite = pygame.image.load("assets/artRio.png")
+        self.font = pygame.font.Font("assets/font.ttf", 30)
