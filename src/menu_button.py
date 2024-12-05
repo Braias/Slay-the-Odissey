f036b81fd5acd33e9bc20960cfb2cc269c1ed960
Nicholas2104 <nicholasnogueira.costa@gmail.com>
2024-12-04 22:59:05 -0300

    consertando indentacao

--- src/menu_button.py
+++ src/menu_button.py
@@ -15,11 +15,11 @@ class Button():
         self.text_pos = pos - pygame.Vector2(self.text_surface.get_size()) / 2
         self.is_hovering = False
 
-	def draw(self, surface: pygame.Surface):
-		surface.blit(self.button_surface, self.pos)
-		self.text_surface.set_alpha(255 if self.is_hovering else 150)
-		surface.blit(self.text_surface, self.text_pos)
+    def draw(self, surface: pygame.Surface):
+        surface.blit(self.button_surface, self.pos)
+        self.text_surface.set_alpha(255 if self.is_hovering else 150)
+        surface.blit(self.text_surface, self.text_pos)
 
-	def on_mouse_motion(self, mouse_pos: pygame.Vector2):
-		self.is_hovering = self.pos.x < mouse_pos.x < self.pos.x + self.size.x \
-			and self.pos.y < mouse_pos.y < self.pos.y + self.size.y
+    def on_mouse_motion(self, mouse_pos: pygame.Vector2):
+        self.is_hovering = self.pos.x < mouse_pos.x < self.pos.x + self.size.x \
+            and self.pos.y < mouse_pos.y < self.pos.y + self.size.y
